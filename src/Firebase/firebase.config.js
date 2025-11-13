fcef6871bc6fc8034be14221045b001ed43a9d08
nabilnir <nirarhan@gmail.com>
2025-11-13 18:31:45 +0600

    firebase added

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// src/firebase.config.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+console.log("VITE_API_KEY:", import.meta.env.VITE_API_KEY);
+console.log("VITE_STORAGE_BUCKET:", import.meta.env.VITE_STORAGE_BUCKET);
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET, // check this value
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
