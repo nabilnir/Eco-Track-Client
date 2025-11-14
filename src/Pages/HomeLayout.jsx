637f120fcb515c7319db9fae6f2aee89b131bbb2
nabilnir <nirarhan@gmail.com>
2025-11-14 10:18:11 +0600

    Hero banner and spinner added

---
+++ src/Pages/HomeLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import HeroBanner from '../components/Home/HeroBanner';
+
+const HomeLayout = () => {
+    return (
+        <div>
+            <HeroBanner />
+        </div>
+    );
+};
+
+export default HomeLayout;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -2,12 +2,17 @@ import { createBrowserRouter } from "react-router";
 import Root from "../Root/Root";
 import Login from "../Pages/Login";
 import Register from "../Pages/Register";
+import HomeLayout from "../Pages/HomeLayout";
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: Root,
     children : [
+      {
+           path : '/',
+           Component : HomeLayout
+      },
       {
         path : '/login',
         Component : Login
